a394e7eea8e802d24660cb9a35c65dc7ab30cfcc
katherinepatinos <124631450+katherinepatinos@users.noreply.github.com>
2023-03-05 23:49:14 -0500

    Se elimina proyecto
    Proyecto eliminado del main

--- HumanResources/src/co/edu/unbosque/controller/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Paquete controlador
- * @author Katherine Patino
- * @author Daniela Pineros
- */
-package co.edu.unbosque.controller;
--- HumanResources/src/co/edu/unbosque/model/persistence/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Paquete de persistencia
- * @author Katherine Patino
- * @author Daniela Pineros
- */
-package co.edu.unbosque.model.persistence;
